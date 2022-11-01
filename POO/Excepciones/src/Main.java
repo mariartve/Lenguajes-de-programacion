3850ae166f58d33d8108c0b217bf751d4ed1de2d
mariartve <110705321+mariartve@users.noreply.github.com>
2022-11-01 02:23:25 -0600

    Update Main.java

--- POO/Excepciones/src/Main.java
+++ POO/Excepciones/src/Main.java
@@ -12,3 +12,9 @@ public class Main {
         System.out.println("El resultado de la operación es: "+e.resultado(expresion).getNum());
     }
 }
+
+/*Resultado:
+1. Operacion: 512 * 30 / 2: El resultado de la operación es: 7680
+2. Operacion: 512x * 30 / 2: Error, la operación contiene un carácter inválido, El resultado de la operación es: -1
+3. Operacion: 512 * + 30 / 2: Los elementos de la expresión no están ordenados en infijo, El resultado de la operación es: -1
+*/
