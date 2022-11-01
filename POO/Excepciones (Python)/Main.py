9b485634ba2b86d1a138c42b07ce60b42df4b331
mariartve <110705321+mariartve@users.noreply.github.com>
2022-11-01 02:26:21 -0600

    Update Main.py

--- POO/Excepciones (Python)/Main.py
+++ POO/Excepciones (Python)/Main.py
@@ -8,3 +8,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+"""Resultado:
+1. Operacion: 512 * 30 / 2: El resultado de la operación es:  7680.0
+2. Operacion: 512x * 30 / 2: Error, la operación contiene un carácter inválido, El resultado de la operación es: -1
+3. Operacion: 512 * + 30 / 2: Los elementos de la expresión no están ordenados en infijo, El resultado de la operación es: -1
+"""
